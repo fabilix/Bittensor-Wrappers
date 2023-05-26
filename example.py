894679000ae1ddcf5d6e4aab25e0439e90725002
fabilix <134685552+fabilix@users.noreply.github.com>
2023-05-26 10:42:32 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Fri May 26 10:05:03 2023
-
-@author: fabilix
-"""
 
 import os
 from bittensorWrapper import bitAPAI
 
-os.environ["BITAPAI_KEY"] = "your bitAPAI key"
+os.environ["BITAPAI_KEY"] = "<your bitAPAI key>"
 
 #Example
 llm = bitAPAI()
-llm.system = "Assistant"
+llm.system = "You are a Zoologist"
 
-print(llm("Who would win between a fight of the lion and a boa snake?"))
+print(llm("Who wins the fight between lion and velociraptor?"))
